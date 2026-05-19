9cfc508214152fe9e51f8338073e80e0a2111344
thaiidduong <ah_thduong1507@icloud.com>
2026-05-19 21:27:32 +0700

    Update DASH project with multi-video support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -7,7 +8,16 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static('htdocs'));
+// Serve thư mục giao diện
+app.use(express.static(path.join(__dirname, 'htdocs')));
+
+// Serve video DASH
+app.use('/video', express.static(path.join(__dirname, 'video_input')));
+
+// Route mặc định
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'htdocs', 'index.html'));
+});
 
 app.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
